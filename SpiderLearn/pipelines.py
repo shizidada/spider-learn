5886f80745d7fc8989745950cb967b406b1b9658
shizi <wb-jj197972@alibaba-inc.com>
2018-06-22 11:15:51 +0800

    py 3

--- SpiderLearn/pipelines.py
+++ SpiderLearn/pipelines.py
@@ -3,7 +3,7 @@
 # Define your item pipelines here
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+# See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
 
 class SpiderlearnPipeline(object):
